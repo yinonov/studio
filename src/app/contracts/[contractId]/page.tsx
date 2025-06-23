e42ce7ba08bd4a392a636965a2d961ad1e272efd
Yinon Oved <yinonoved@gmail.com>
2025-06-23 20:29:54 +0000

    there's no action to trigger the signing process

--- src/app/contracts/[contractId]/page.tsx
+++ src/app/contracts/[contractId]/page.tsx
@@ -1,3 +1,4 @@
+
 "use client";
 
 import { useEffect, useState, useRef, useCallback } from "react";
@@ -898,3 +899,5 @@ export default function ContractViewPage() {
     </section>
   );
 }
+
+    
