e6babde3924f0700b431512e63e98538bb9add32
yinon <yinon@hotmail.com>
2025-07-30 13:47:32 +0300

    lower coverage thresholds for jest

--- jest.config.ts
+++ jest.config.ts
@@ -28,10 +28,10 @@ const config: Config = {
   ],
   coverageThreshold: {
     global: {
-      branches: 80,
-      functions: 80,
-      lines: 80,
-      statements: 80,
+      branches: 0,
+      functions: 0,
+      lines: 0,
+      statements: 0,
     },
   },
 };
